d849f72a7d7087bac35a2a87675a40ffab2337a2
Hao Chen <chenhaoa@uniontech.com>
2021-06-28 16:29:25 +0800

    kobject: add hello kobject
    Signed-off-by: Hao Chen <chenhaoa@uniontech.com>

--- hello.c
+++ hello.c
@@ -1,9 +1,18 @@
+#include <linux/kobject.h>
 #include <linux/module.h>/* Needed by all modules */
 #include <linux/kernel.h>/* Needed for KERN_ALERT */
 #include <linux/init.h>/*Needed for __init */
 
+struct kobject *hello_kobj;
+
 static int __init test_init(void)
 {
+	hello_kobj = kobject_create_and_add("hello", NULL);
+	if (!hello_kobj)
+		return -ENOMEM;
+
+	kobject_uevent(hello_kobj, KOBJ_ADD);
+
 	printk(KERN_ALERT"Hello world!\n");
 
 	return 0;
@@ -12,6 +21,8 @@ static int __init test_init(void)
 static void __exit test_exit(void)
 
 {
+	kobject_del(hello_kobj);
+
 	printk(KERN_ALERT"Goodbye world!\n");
 }
 
